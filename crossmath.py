44508de75c066414922977345a7f0dfc277b02cd
Márton Somogyi <kamarton@gmail.com>
2024-03-09 17:17:38 +0100

    eng: crossmath generate

--- crossmath.py
+++ crossmath.py
@@ -1,3 +1,4 @@
+import random
 import time
 
 from expression import (
@@ -14,15 +15,17 @@ class CrossMath:
         self._items = ExpressionItems()
         self._expression_resolver = ExpressionResolver()
 
+    def _init_generate(self):
+        expression = self._expression_resolver.resolve(Expression())
+        if expression is None:
+            raise Exception("No expression found")
+        direction = random.choice([Direction.HORIZONTAL, Direction.VERTICAL])
+        self._items.append(ExpressionItem(0, 0, direction, expression))
+
     def generate(self):
         start_time = time.time()
-
+        self._init_generate()
         for i in range(1):
-            expression = Expression()
-            expression = self._expression_resolver.resolve(expression)
-            if expression is None:
-                continue
-            self._items.append(ExpressionItem(0, 0, Direction.HORIZONTAL, expression))
             print("time: ", time.time() - start_time)
 
     def print(self):
